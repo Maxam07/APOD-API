26919862c863e119dc0808b680e2a3be832423b9
Max U <maxuberas@gmail.com>
2024-05-04 22:39:25 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('./public'));
+
+// Set EJS as view engine
+app.set("view engine", "ejs");
+app.set('views, __dirname + ./views');
+
+// API KEY AUTHORIZATION FROM NASA https://api.nasa.gov/
+const yourAPIkey = "MUd0cY2A5pCMikTtnb65hAIh24LpPAR4Bdp1H5if";
+
+// API Collection
+const Apod_API = "https://api.nasa.gov/planetary/apod?";
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get(Apod_API + `api_key=${yourAPIkey}`);
+        console.log("Working");
+        const finalRes = result.data;
+        res.render('index', { finalRes }); // No need to specify file extension
+    } catch (error) {
+        const message = "Failed to make request";
+        console.error(message);
+        res.render('index', { error: message }); // No need to specify file extension
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server Listening to ${port}`);
+});
